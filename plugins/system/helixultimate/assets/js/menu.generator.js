a04f6707cc8b146a18696f93f6884eb70e104a87
JoomShaper <support@joomshaper.com>
2018-01-09 16:17:02 +0600

    Copyright

--- plugins/system/helixultimate/assets/js/menu.generator.js
+++ plugins/system/helixultimate/assets/js/menu.generator.js
@@ -1,7 +1,7 @@
 /**
-* @package Helix3 Framework
+* @package Helix Ultimate Framework
 * @author JoomShaper http://www.joomshaper.com
-* @copyright Copyright (c) 2010 - 2015 JoomShaper
+* @copyright Copyright (c) 2010 - 2018 JoomShaper
 * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
 */
 
--- plugins/system/helixultimate/fields/menulayout.php
+++ plugins/system/helixultimate/fields/menulayout.php
@@ -1,12 +1,11 @@
 <?php
 /**
-* @package Helix3 Framework
+* @package Helix Ultimate Framework
 * @author JoomShaper http://www.joomshaper.com
-* @copyright Copyright (c) 2010 - 2015 JoomShaper
+* @copyright Copyright (c) 2010 - 2018 JoomShaper
 * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
 */
 
-//no direct accees
 defined ('_JEXEC') or die ('resticted aceess');
 
 require_once dirname(__DIR__) . '/core/lib/fa.php';

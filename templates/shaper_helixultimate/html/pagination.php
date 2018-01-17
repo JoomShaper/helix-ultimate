5638fbc96923a72c3211c093cb74d849c181d479
Nayem Majhar <nayemmajhar@gmail.com>
2018-01-17 12:46:59 +0600

    template spelling mistake fixed

--- templates/shaper_helixultimate/html/pagination.php
+++ templates/shaper_helixultimate/html/pagination.php
@@ -5,8 +5,8 @@
  * @copyright Copyright (c) 2010 - 2015 JoomShaper
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or later
 */
-//no direct accees
-defined ('_JEXEC') or die ('resticted aceess');
+
+defined ('_JEXEC') or die ('Restricted access');
 
 function pagination_list_render($list) {
 	// Initialize variables

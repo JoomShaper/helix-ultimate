577989e989814e602c720648fb1247fb5d11bd88
Nayem Majhar <nayemmajhar@gmail.com>
2018-01-19 17:37:01 +0600

    device responsive issue fixed

--- plugins/system/helixultimate/layouts/frontend/rows.php
+++ plugins/system/helixultimate/layouts/frontend/rows.php
@@ -7,50 +7,29 @@
 */
 defined('_JEXEC') or die('Restricted Access');
 
-//helper & model
 $layout_path_carea  = JPATH_ROOT .'/plugins/system/helixultimate/layouts';
 $layout_path_module = JPATH_ROOT .'/plugins/system/helixultimate/layouts';
 
 $data = $displayData;
 
 $output ='';
-
 $output .= '<div class="row">';
 
-foreach ($data['rowColumns'] as $key => $column){
-
-    //Responsive Utilities
-    if (isset($column->settings->xs_col) && $column->settings->xs_col) {
-        $column->className = $column->settings->xs_col . ' ' . $column->className;
-    }
-
-    if (isset($column->settings->sm_col) && $column->settings->sm_col) {
-        $column->className = preg_replace('/col-sm-\d*/', $column->settings->sm_col, $column->className);
-    }
-
-    if (isset($column->settings->hidden_md) && $column->settings->hidden_md) {
-        $column->className = $column->className . ' hidden-md hidden-lg';
-    }
-
-    if (isset($column->settings->hidden_sm) && $column->settings->hidden_sm) {
-        $column->className = $column->className . ' hidden-sm';
-    }
-
-    if (isset($column->settings->hidden_xs) && $column->settings->hidden_xs) {
-        $column->className = $column->className . ' hidden-xs';
-    }
-    //End Responsive Utilities
+foreach ($data['rowColumns'] as $key => $column)
+{
     $column->hasFeature = $data['loadFeature'];
-    if ($column->settings->column_type){ //Component
+    if ($column->settings->column_type)
+    {
         $getLayout = new JLayoutFile('frontend.conponentarea', $layout_path_carea );
         $output .= $getLayout->render($column);
     }
-    else { // Module
+    else
+    {
         $getLayout = new JLayoutFile('frontend.modules', $layout_path_module );
         $output .= $getLayout->render($column);
     }
 }
 
-$output .= '</div>'; //.row
+$output .= '</div>';
 
 echo $output;

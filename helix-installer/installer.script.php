fa786f8370ef1f0df892bc6bdb2ce4c1bfb75e79
JoomShaper <support@joomshaper.com>
2018-01-25 18:06:36 +0600

    Installer updated

--- helix-installer/installer.script.php
+++ helix-installer/installer.script.php
@@ -11,7 +11,8 @@ defined ('_JEXEC') or die();
 class plgSystemTmplHelixUltimateInstallerScript {
 
     function postflight($type, $parent) {
-        $db = \JFactory::getDBO();
+        
+        $db = JFactory::getDBO();
         $status = new stdClass;
         $status->plugins = array();
 
@@ -24,49 +25,72 @@ class plgSystemTmplHelixUltimateInstallerScript {
             $group = (string)$plugin->attributes()->group;
             $path = $src.'/plugins/'.$group;
 
-            if (\JFolder::exists($src.'/plugins/'.$group.'/'.$name))
+            if (JFolder::exists($src.'/plugins/'.$group.'/'.$name))
             {
                 $path = $src.'/plugins/'.$group.'/'.$name;
             }
 
-            $installer = \JInstaller::getInstance();
+            $installer = new JInstaller;
             $result = $installer->install($path);
 
             if ($result) {
-                $db = \JFactory::getDbo();
                 $query = $db->getQuery(true);
-                
                 $fields = array(
                     $db->quoteName('enabled') . ' = 1'
                 );
-                
+
                 $conditions = array(
-                    $db->quoteName('type') . ' = ' . $db->quote('plugin'),
+                    $db->quoteName('type') . ' = ' . $db->quote('plugin'), 
                     $db->quoteName('element') . ' = ' . $db->quote($name),
                     $db->quoteName('folder') . ' = ' . $db->quote($group)
                 );
-                
+
                 $query->update($db->quoteName('#__extensions'))->set($fields)->where($conditions);
                 $db->setQuery($query);
                 $db->execute();
             }
+
         }
 
-        $template_path = $src . '/template';
+        $templates = $manifest->xpath('template');
 
-        if (\JFolder::exists( $template_path )) {
-            $installer = \JInstaller::getInstance();
-            $installer->install($template_path);
+        foreach($templates as $key => $template)
+        {
+            $tmpl_name = (string)$template->attributes()->name;
+            $template_path = $src.'/template';
+            $options_default = file_get_contents($template_path .'/layout/default.json');
+
+            if (JFolder::exists( $template_path )) {
+                $installer = new JInstaller;
+                $result = $installer->install($template_path);
+
+                if ($result && $type !== 'update')
+                {
+                    $query = $db->getQuery(true);
+                    $fields = array(
+                        $db->quoteName('params') . ' = ' . $db->quote($options_default)
+                    );
+
+                    $conditions = array(
+                        $db->quoteName('client_id') . ' = 0', 
+                        $db->quoteName('template') . ' = ' . $db->quote($tmpl_name)
+                    );
+
+                    $query->update($db->quoteName('#__template_styles'))->set($fields)->where($conditions);
+                    $db->setQuery($query);
+                    $db->execute();
+                }
+            }
         }
 
-        $conf = \JFactory::getConfig();
+        $conf = JFactory::getConfig();
         $conf->set('debug', false);
         $parent->getParent()->abort();
     }
 
     public function abort($msg = null, $type = null){
         if ($msg) {
-            \JError::raiseWarning(100, $msg);
+            JError::raiseWarning(100, $msg);
         }
         foreach ($this->packages as $package) {
             $package['installer']->abort(null, $type);

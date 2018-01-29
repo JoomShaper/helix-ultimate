7843ebce83d62f8c5dd323c1ad3f29437ae973c1
JoomShaper <support@joomshaper.com>
2018-01-30 01:34:29 +0600

    Added content-top, content-bottom positions

--- plugins/system/helixultimate/layouts/frontend/conponentarea.php
+++ plugins/system/helixultimate/layouts/frontend/conponentarea.php
@@ -7,13 +7,31 @@
 
 defined('_JEXEC') or die('Restricted Access');
 
+$doc = \JFactory::getDocument();
+
 $data = $displayData;
 
 $output ='';
 $output .= '<div id="sp-component" class="' . $data->settings->className . '">';
 $output .= '<div class="sp-column ' . ($data->settings->custom_class) . '">';
 $output .= '<jdoc:include type="message" />';
+
+if($doc->countModules('content-top'))
+{
+    $output .= '<div class="sp-module-content-top clearfix">';
+    $output .= '<jdoc:include type="modules" name="content-top" style="sp_xhtml" />';
+    $output .= '</div>';
+}
+
 $output .= '<jdoc:include type="component" />';
+
+if($doc->countModules('content-bottom'))
+{
+    $output .= '<div class="sp-module-content-top clearfix">';
+    $output .= '<jdoc:include type="modules" name="content-bottom" style="sp_xhtml" />';
+    $output .= '</div>';
+}
+
 $output .= '</div>';
 $output .= '</div>';
 

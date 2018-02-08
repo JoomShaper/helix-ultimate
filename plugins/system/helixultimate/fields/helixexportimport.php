5a86488cac00934b654d070834f3f3c5c7053b36
JoomShaper <support@joomshaper.com>
2018-02-09 03:21:57 +0600

    Improved language

--- plugins/system/helixultimate/fields/helixexportimport.php
+++ plugins/system/helixultimate/fields/helixexportimport.php
@@ -22,11 +22,11 @@ class JFormFieldHelixexportimport extends JFormField
 
 		$output  = '';
 		$output .= '<div class="import-export clearfix" style="margin-bottom:30px;">';
-		$output .= '<a class="btn btn-success" target="_blank" href="'. $export_url .'">'. JText::_("HELIX_SETTINGS_EXPORT") .'</a>';
+		$output .= '<a class="btn btn-success" target="_blank" href="'. $export_url .'">'. JText::_("HELIX_ULTIMATE_SETTINGS_EXPORT") .'</a>';
 		$output .= '</div>';
 		$output .= '<div class="import-export clearfix">';
 		$output .= '<textarea id="import-data" name="import-data" rows="5" style="margin-bottom:20px;"></textarea>';
-		$output .= '<a id="import-settings" class="btn btn-primary" data-template_id="'. $template_id .'" target="_blank" href="#">'. JText::_("HELIX_SETTINGS_IMPORT") .'</a>';
+		$output .= '<a id="import-settings" class="btn btn-primary" data-template_id="'. $template_id .'" target="_blank" href="#">'. JText::_("HELIX_ULTIMATE_SETTINGS_IMPORT") .'</a>';
 		$output .= '</div>';
 
 		return $output;

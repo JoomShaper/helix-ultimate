ee4f06a8b7fa15fec8fefafb81d517dc9888be0a
JoomShaper <support@joomshaper.com>
2018-04-10 16:47:16 +0600

    Empty open graph

--- templates/shaper_helixultimate/html/layouts/joomla/content/open_graph.php
+++ templates/shaper_helixultimate/html/layouts/joomla/content/open_graph.php
@@ -19,12 +19,30 @@ $doc->addCustomTag('<meta property="og:type" content="article" />');
 $doc->addCustomTag('<meta property="og:url" content="'. \JURI::current() . '" />');
 $doc->addCustomTag('<meta property="og:title" content="'. htmlspecialchars($title ) .'" />');
 $doc->addCustomTag('<meta property="og:description" content="'. \JHtml::_('string.truncate', (strip_tags($content)), 150) .'" />');
-$doc->addCustomTag('<meta property="og:image" content="'. \JURI::root().ltrim($image, '/') .'" />');
-$doc->addCustomTag('<meta property="fb:app_id" content="'. (int) $fb_app_id . '" />');
+
+if(isset($image) && $image)
+{
+    $doc->addCustomTag('<meta property="og:image" content="'. \JURI::root().ltrim($image, '/') .'" />');
+}
+
+if(isset($fb_app_id) && $fb_app_id)
+{
+    $doc->addCustomTag('<meta property="fb:app_id" content="'. (int) $fb_app_id . '" />');
+}
+
 $doc->addCustomTag('<meta property="og:site_name" content="'. htmlspecialchars($sitename) .'" />');
 
 // Twitter
 $doc->addCustomTag('<meta name="twitter:description" content="'. \JHtml::_('string.truncate', (strip_tags($content)), 150) .'" />');
-$doc->addCustomTag('<meta name="twitter:image:src" content="'. \JURI::root().ltrim($image, '/') .'" />');
-$doc->addCustomTag('<meta name="twitter:site" content="@'. htmlspecialchars($twitter_site) .'" />');
+
+if(isset($image) && $image)
+{
+    $doc->addCustomTag('<meta name="twitter:image:src" content="'. \JURI::root().ltrim($image, '/') .'" />');
+}
+
+if(isset($twitter_site) && $twitter_site)
+{
+    $doc->addCustomTag('<meta name="twitter:site" content="@'. htmlspecialchars($twitter_site) .'" />');
+}
+
 $doc->addCustomTag('<meta name="twitter:card" content="summary_large_image" />');

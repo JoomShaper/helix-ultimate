689a1b218905b79871bdd73556ab4a2b00c09aac
kawshar <kawshar@joomshaper.com>
2021-01-10 04:42:44 +0600

    menu builder improvement

--- plugins/system/helixultimate/src/HttpRequests/Response.php
+++ plugins/system/helixultimate/src/HttpRequests/Response.php
@@ -153,6 +153,16 @@ class Response
 				$html[] = '		<div class="hu-branch-drag-handler">';
 				$html[] = '			<span class="hu-branch-icon"><svg width="6" height="10" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx=".904" cy=".904" r=".904" /><circle cx=".904" cy="4.7" r=".904" /><circle cx=".904" cy="8.496" r=".904" /><circle cx="4.7" cy=".904" r=".904" /><circle cx="4.7" cy="4.7" r=".904" /><circle cx="4.7" cy="8.496" r=".904" /></svg></span>';
 				$html[] = '			<span class="hu-branch-title">' . $item->title . '</span>';
+
+				$html[] = '			<span class="hu-branch-tools">';
+				$html[] = '				<svg xmlns="http://www.w3.org/2000/svg" width="15" height="3" fill="none"><path fill-rule="evenodd" d="M3 1.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zm6 0a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM13.5 3a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" clip-rule="evenodd"></path></svg>';
+				$html[] = '				<ul>';
+				$html[] = '					<li><a href="#">Edit</a></li>';
+				$html[] = '					<li><a href="#">Delete</a></li>';
+				$html[] = '					<li><a href="#">Mega Menu</a></li>';
+				$html[] = '				</ul>';
+				$html[] = '			</span>';
+
 				$html[] = '		</div>';
 				$html[] = '	</div>';
 				$html[] = '<div class="hu-menu-children-bus"></div>';

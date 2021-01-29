ddbf4322f20c4a102c8b345df4efb067587f2911
Al Mamun <mamunhpath@hotmail.com>
2021-01-29 18:10:44 +0600

    disable menu style updated

--- plugins/system/helixultimate/layout/megaMenu/grid.php
+++ plugins/system/helixultimate/layout/megaMenu/grid.php
@@ -78,19 +78,7 @@ $modules = Helper::getModules();
             </div>
 
             <div class="hu-megamenu-modules-container">
-                <!-- <?php if (!empty($modules)): ?>
-                    <div class="row">
-                        <?php foreach ($modules as $module): ?>
-                            <div class="col-4 hu-megamenu-column">
-                                <div class="hu-megamenu-module-item">
-                                    <strong class="hu-megamenu-module-title"><?php echo $module->title; ?></strong>
-                                    <p class="hu-megamenu-module-desc"><?php echo (strlen($module->desc) > 80 ? substr($module->desc, 0, 80) . '...' : $module->desc); ?></p>
-                                    <button type="button" role="button" class="hu-btn hu-btn-default hu-megamenu-insert-module" data-module="<?php echo $module->id; ?>"><?php echo Text::_('HELIX_ULTIMATE_MODULE_INSERT'); ?></button>
-                                </div>
-                            </div>
-                        <?php endforeach ?>
-                    </div>
-                <?php endif ?> -->
+               
             </div>
         </div>
     </div>

c55770552fca21a2a6f9e84eca244dd064f9d8e2
Sajeeb Ahamed <sajeeb07ahamed@gmail.com>
2021-02-18 22:37:08 +0600

    :bulb: add new offcanvas styles, add new options for handling offcanvas search, social, contact

---
+++ templates/shaper_helixultimate/offcanvas/style-2/canvas.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package Helix_Ultimate_Framework
+ * @author JoomShaper <support@joomshaper.com>
+ * @copyright Copyright (c) 2010 - 2020 JoomShaper
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
+ */
+
+defined ('_JEXEC') or die('Restricted Access');
+
+use HelixUltimate\Framework\Platform\Helper;
+use Joomla\CMS\Helper\ModuleHelper;
+
+$params = $displayData->params;
+$template = $displayData->template->template;
+
+include_once JPATH_THEMES . '/' . $template . '/features/menu.php';
+include_once JPATH_THEMES . '/' . $template . '/features/social.php';
+include_once JPATH_THEMES . '/' . $template . '/features/contact.php';
+include_once JPATH_THEMES . '/' . $template . '/features/logo.php';
+
+$menu = new HelixUltimateFeatureMenu($params);
+$social = new HelixUltimateFeatureSocial($params);
+$contact = new HelixUltimateFeatureContact($params);
+$logo = new HelixUltimateFeatureLogo($params);
+
+$menuModule = Helper::createModule('mod_menu', [
+	'title' => 'Main Menu',
+	'params' => '{"menutype":"' . $params->get('offcanvas_menu', 'mainmenu') . '","base":"","startLevel":"1","endLevel":"0","showAllChildren":"1","tag_id":"","class_sfx":" nav-pills","window_open":"","layout":"_:default","moduleclass_sfx":"","cache":"1","cache_time":"900","cachemode":"itemid","module_tag":"div","bootstrap_size":"0","header_tag":"h3","header_class":"","style":"0"}',
+	'name' => 'menu'
+]);
+
+$searchModule = Helper::getSearchModule();
+?>
+<div class="offcanvas-inner">
+	<div class="d-flex header-modules">
+		<?php if ($params->get('offcanvas_enable_search', 0)): ?>
+			<?php echo ModuleHelper::renderModule($searchModule, ['style' => 'sp_xhtml']); ?>
+		<?php endif ?>
+
+		<?php if ($params->get('offcanvas_enable_login', 0)): ?>
+			<?php echo $menu->renderLogin(); ?>
+		<?php endif ?>
+	</div>
+
+	<?php echo ModuleHelper::renderModule($menuModule, ['style' => 'sp_xhtml']); ?>
+
+	<?php if ($params->get('offcanvas_enable_social')): ?>
+		<?php echo $social->renderFeature(); ?>
+	<?php endif ?>
+
+	<?php if ($params->get('offcanvas_enable_contact')): ?>
+		<?php echo $contact->renderFeature(); ?>
+	<?php endif ?>
+</div>

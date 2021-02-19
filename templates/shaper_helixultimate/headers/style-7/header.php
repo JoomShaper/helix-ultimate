500ae65484db3a745985f3bed365073a6fa95f6b
amjad rana <amjadrana@amjads-iMac.local>
2021-02-19 12:56:12 +0600

    modal toggler

--- templates/shaper_helixultimate/headers/style-7/header.php
+++ templates/shaper_helixultimate/headers/style-7/header.php
@@ -79,12 +79,11 @@ $searchModule = Helper::getSearchModule();
 
 					<!-- Modal menu toggler -->
 					<a id="modal-menu-toggler" aria-label="' . JText::_('HELIX_ULTIMATE_NAVIGATION') . '" class="ml-3" href="#">
-						<div class="open-icon">
-							<?php echo JText::_('HELIX_ULTIMATE_MENU_TEXT');?> <i class="fas fa-bars" aria-hidden="true"></i>
+						<div class="burger-icon" action-open-off-canvas="">
+							<span></span>
+							<span></span>
+							<span></span>
 						</div>
-						<span class="close-icon">
-							<?php echo JText::_('HELIX_ULTIMATE_CLOSE');?> <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg></i>
-						</span>
 					</a>
 
 					<!-- Modal menu -->

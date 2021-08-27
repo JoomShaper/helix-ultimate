7681f299444c39e4f6ac55a896b245a0d490b183
kawshar <kawshar@joomshaper.com>
2021-08-27 20:39:16 +0600

    updated chromes and paginations

--- templates/shaper_helixultimate/html/modules.php
+++ templates/shaper_helixultimate/html/modules.php
@@ -8,27 +8,4 @@
 
 defined ('_JEXEC') or die();
 
-function modChrome_sp_xhtml($module, $params, $attribs)
-{
-
-	$moduleTag     = htmlspecialchars($params->get('module_tag', 'div'), ENT_QUOTES, 'UTF-8');
-	$bootstrapSize = (int) $params->get('bootstrap_size', 0);
-	$moduleClass   = $bootstrapSize !== 0 ? ' span' . $bootstrapSize : '';
-	$headerTag     = htmlspecialchars($params->get('header_tag', 'h3'), ENT_QUOTES, 'UTF-8');
-	$headerClass   = htmlspecialchars($params->get('header_class', 'sp-module-title'), ENT_COMPAT, 'UTF-8');
-
-	if ($module->content)
-	{
-		echo '<' . $moduleTag . ' class="sp-module ' . htmlspecialchars($params->get('moduleclass_sfx'), ENT_COMPAT, 'UTF-8') . $moduleClass . '">';
-
-			if ($module->showtitle)
-			{
-				echo '<' . $headerTag . ' class="' . $headerClass . '">' . $module->title . '</' . $headerTag . '>';
-			}
-
-			echo '<div class="sp-module-content">';
-			echo $module->content;
-			echo '</div>';
-		echo '</' . $moduleTag . '>';
-	}
-}
+require HelixUltimate\Framework\Platform\HTMLOverride::loadTemplate();

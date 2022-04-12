5cd105159879f6f22a0fd801c70c278220b84666
prakash-a7x <prakash.a7x@gmail.com>
2022-04-12 15:04:24 +0600

    full image j4 attributes issue fix

--- plugins/system/helixultimate/overrides/layouts/joomla/content/full_image.php
+++ plugins/system/helixultimate/overrides/layouts/joomla/content/full_image.php
@@ -44,17 +44,56 @@ if(isset($attribs->helix_ultimate_image) && $attribs->helix_ultimate_image != ''
 ?>
 <?php if($full_image) : ?>
 	<div class="article-full-image">
-		<img src="<?php echo $full_image; ?>" alt="<?php echo htmlspecialchars(isset($attribs->helix_ultimate_image_alt_txt) ? $attribs->helix_ultimate_image_alt_txt : $displayData->title, ENT_COMPAT, 'UTF-8'); ?>" itemprop="image">
+		<?php 
+		if (JVERSION >= 4)
+		{
+			$layoutAttr = [
+				'src'      => $full_image,
+				'itemprop' => 'image',
+				'alt'      => htmlspecialchars(isset($attribs->helix_ultimate_image_alt_txt) ? $attribs->helix_ultimate_image_alt_txt : $displayData->title, ENT_COMPAT, 'UTF-8'),
+			];
+
+			echo LayoutHelper::render('joomla.html.image', $layoutAttr);
+		}
+		else
+		{
+		?>
+			<img src="<?php echo $full_image; ?>" alt="<?php echo htmlspecialchars(isset($attribs->helix_ultimate_image_alt_txt) ? $attribs->helix_ultimate_image_alt_txt : $displayData->title, ENT_COMPAT, 'UTF-8'); ?>" itemprop="image">
+		<?php
+		}
+		?>
 	</div>
 <?php else: ?>
 	<?php $images = json_decode($displayData->images); ?>
 	<?php if (isset($images->image_fulltext) && !empty($images->image_fulltext)) : ?>
 		<?php $imgfloat = empty($images->float_fulltext) ? $params->get('float_fulltext') : $images->float_fulltext; ?>
 		<div class="article-full-image float-<?php echo htmlspecialchars($imgfloat); ?>">
-			<img <?php if ($images->image_fulltext_caption) :
+			<?php 
+			if (JVERSION >= 4)
+			{
+				$layoutAttr = [
+					'src'      => htmlspecialchars($images->image_fulltext),
+					'itemprop' => 'image',
+					'alt'      => htmlspecialchars(isset($attribs->helix_ultimate_image_alt_txt) ? $attribs->helix_ultimate_image_alt_txt : $images->image_fulltext_alt),
+				];
+				if (isset($images->image_fulltext_caption) && $images->image_fulltext_caption !== '') 
+				{
+					$layoutAttr['class'] = 'caption';
+					$layoutAttr['title'] = htmlspecialchars($images->image_fulltext_caption);
+				}
+
+				echo LayoutHelper::render('joomla.html.image', $layoutAttr);
+			}
+			else
+			{
+			?>
+				<img <?php if ($images->image_fulltext_caption) :
 				echo 'class="caption"' . ' title="' . htmlspecialchars($images->image_fulltext_caption) . '"';
 				endif; ?>
-				src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars(isset($attribs->helix_ultimate_image_alt_txt) ? $attribs->helix_ultimate_image_alt_txt : $images->image_fulltext_alt); ?>" itemprop="image"> 
+				src="<?php echo htmlspecialchars($images->image_fulltext); ?>" alt="<?php echo htmlspecialchars(isset($attribs->helix_ultimate_image_alt_txt) ? $attribs->helix_ultimate_image_alt_txt : $images->image_fulltext_alt); ?>" itemprop="image">
+			<?php 
+			}
+			?>
 		</div>
 	<?php endif; ?>
 <?php endif; ?>

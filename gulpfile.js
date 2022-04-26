22b5ac8149c70f443ae0b8f88fc908d08c7ea1ac
Amjad Rana <amjadrana@Amjads-MacBook-Pro.local>
2022-04-26 13:46:39 +0600

    version updated

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,10 @@ const config = {
     srcPath: path.resolve(__dirname),
     buildPath: path.resolve(__dirname, './package/'),
     qsPath: path.resolve(__dirname, './package/helix_ultimate_quickstart/'),
-    qsPackageName: 'helix_ultimate_quickstart_j4_2.0.7.zip',
-    packageName: 'helix_ultimate_pkg_2.0.7.zip',
-    pluginPackageName: 'helix_ultimate_plugin_pkg_2.0.7.zip',
-    templatePackageName: 'helix_ultimate_template_pkg_2.0.7.zip',
+    qsPackageName: 'helix_ultimate_quickstart_j4_2.0.8.zip',
+    packageName: 'helix_ultimate_pkg_2.0.8.zip',
+    pluginPackageName: 'helix_ultimate_plugin_pkg_2.0.8.zip',
+    templatePackageName: 'helix_ultimate_template_pkg_2.0.8.zip',
     templateFileExtensions: 'xml, json, php, png, scss, js, ico, svg, jpg, eot, ttf, woff, woff2, otf, css, html',
     pluginFileExtensions: function () {
         return this.templateFileExtensions + ', ini';

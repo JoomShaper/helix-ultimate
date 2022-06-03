dec4b6407f06c90433da85df8cca300c9b9aa25b
prakash-a7x <prakash.a7x@gmail.com>
2022-06-03 16:13:34 +0600

    #266, #271 frontend media issue fix

--- plugins/system/helixultimate/overrides/com_content/form/edit.php
+++ plugins/system/helixultimate/overrides/com_content/form/edit.php
@@ -24,6 +24,8 @@ $doc->addStylesheet($cssPath);
 HTMLHelper::_('behavior.keepalive');
 HTMLHelper::_('behavior.formvalidator');
 
+$doc->getWebAssetManager()->useScript('bootstrap.modal');
+
 if (JVERSION < 4)
 {
 	HTMLHelper::_('formbehavior.chosen', '#jform_catid', null, array('disable_search_threshold' => 0));

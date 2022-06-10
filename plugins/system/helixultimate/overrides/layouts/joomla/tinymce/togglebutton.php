df184fe9d731bc9370f09e34d602c26775b3147c
prakash-a7x <prakash.a7x@gmail.com>
2022-06-10 16:38:21 +0600

    blog edit j3  & j4 toggle editor fix

--- plugins/system/helixultimate/overrides/layouts/joomla/tinymce/togglebutton.php
+++ plugins/system/helixultimate/overrides/layouts/joomla/tinymce/togglebutton.php
@@ -9,13 +9,33 @@
 defined('JPATH_BASE') or die;
 
 use Joomla\CMS\Language\Text;
-
-?>
-<div class="toggle-editor btn-toolbar float-end clearfix mt-3">
-	<div class="btn-group">
-		<button type="button" disabled class="btn btn-secondary js-tiny-toggler-button">
-			<span class="icon-eye" aria-hidden="true"></span>
-			<?php echo Text::_('PLG_TINY_BUTTON_TOGGLE_EDITOR'); ?>
-		</button>
+if (JVERSION >= 4)
+{
+	?>
+	<div class="toggle-editor btn-toolbar float-end clearfix mt-3">
+		<div class="btn-group">
+			<button type="button" disabled class="btn btn-secondary js-tiny-toggler-button">
+				<span class="icon-eye" aria-hidden="true"></span>
+				<?php echo Text::_('PLG_TINY_BUTTON_TOGGLE_EDITOR'); ?>
+			</button>
+		</div>
 	</div>
-</div>
+	<?php
+}
+else 
+{
+	$name = $displayData;
+	?>
+	<div class="toggle-editor btn-toolbar pull-right clearfix">
+		<div class="btn-group">
+			<a class="btn btn-secondary" href="#"
+				onclick="tinyMCE.execCommand('mceToggleEditor', false, '<?php echo $name; ?>');return false;"
+				title="<?php echo Text::_('PLG_TINY_BUTTON_TOGGLE_EDITOR'); ?>"
+			>
+				<span class="icon-eye" aria-hidden="true"></span> <?php echo Text::_('PLG_TINY_BUTTON_TOGGLE_EDITOR'); ?>
+			</a>
+		</div>
+	</div>
+	<?php 
+	} 
+?>

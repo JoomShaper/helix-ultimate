ccf5a43a592b2155c2edbef6b9ea3b805ba6f570
prakash-a7x <prakash.a7x@gmail.com>
2022-10-21 15:58:41 +0600

    registerBootstrap model to dispatch

--- plugins/system/helixultimate/helixultimate.php
+++ plugins/system/helixultimate/helixultimate.php
@@ -71,7 +71,7 @@ class  PlgSystemHelixultimate extends CMSPlugin
 	 */
 	public function onAfterInitialise()
 	{
-		$this->registerBootstrap();
+		
 	}
 
 	/**
@@ -360,6 +360,8 @@ class  PlgSystemHelixultimate extends CMSPlugin
 	 */
 	public function onAfterDispatch()
 	{
+		$this->registerBootstrap();
+
 		$option     = $this->app->input->get('option', '', 'STRING');
 		$helix      = $this->app->input->get('helix', '', 'STRING');
 		$view       = $this->app->input->get('view', '', 'STRING');

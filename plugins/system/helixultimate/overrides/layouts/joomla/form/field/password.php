edc2dfe4dfa3d7707ffbb8b0cea106bb573393fd
prakash-a7x <prakash.a7x@gmail.com>
2023-02-01 17:03:51 +0600

    test code removed from login

--- plugins/system/helixultimate/overrides/layouts/joomla/form/field/password.php
+++ plugins/system/helixultimate/overrides/layouts/joomla/form/field/password.php
@@ -70,44 +70,4 @@ $attributes = array(
             <span class="visually-hidden"><?php echo Text::_('JSHOWPASSWORD'); ?></span>
         </button>
 	</div>
-
-	<div class="dropdown mt-3">
-		<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-			Dropdown button
-		</button>
-		<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-			<li><a class="dropdown-item" href="#">Action</a></li>
-			<li><a class="dropdown-item" href="#">Another action</a></li>
-			<li><a class="dropdown-item" href="#">Something else here</a></li>
-		</ul>
-		<span class="d-inline-block" tabindex="0" data-bs-toggle="tooltip" title="Disabled tooltip">
-			<button class="btn btn-primary" type="button" disabled>Disabled button</button>
-		</span>
-		<button type="button" class="btn btn-secondary mt-2" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="Top popover">
-			Popover on top
-		</button>
-		<button type="button" class="btn btn-primary mt-2" data-bs-toggle="modal" data-bs-target="#exampleModal">
-			Launch modal
-		</button>
-	</div>
-	
-</div>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-		<div class="modal-header">
-			<h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-			<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-		</div>
-		<div class="modal-body">
-			...
-		</div>
-		<div class="modal-footer">
-			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-			<button type="button" class="btn btn-primary">Save changes</button>
-		</div>
-		</div>
-	</div>
 </div>

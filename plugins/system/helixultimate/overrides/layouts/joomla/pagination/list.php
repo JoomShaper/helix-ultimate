d219b4a999ae05c1ba9b2509a36ed52be312744c
prakash-a7x <prakash.a7x@gmail.com>
2023-06-06 17:38:04 +0600

    🛠️ fixed pagination list override dulicate li issue

--- plugins/system/helixultimate/overrides/layouts/joomla/pagination/list.php
+++ plugins/system/helixultimate/overrides/layouts/joomla/pagination/list.php
@@ -17,12 +17,13 @@ $endDisabled   = $list['end']['active'] ? '' : ' disabled';
 
 ?>
 <ul class="pagination ms-0 mb-4">
-	<li class="pagination-start<?php echo $startDisabled; ?> page-item"><?php echo $list['start']['data']; ?></li>
-	<li class="pagination-prev<?php echo $prevDisabled; ?> page-item"><?php echo $list['previous']['data']; ?></li>
+	<?php echo $list['start']['data']; ?>
+	<?php echo $list['previous']['data']; ?>
+
 	<?php foreach ($list['pages'] as $page) : ?>
-		<?php $disabled = $page['active'] ? '' : ' disabled'; ?>
-		<?php echo '<li class="page-item' . $disabled . '">' . $page['data'] . '</li>'; ?>
+		<?php echo $page['data']; ?>
 	<?php endforeach; ?>
-	<li class="pagination-next<?php echo $nextDisabled; ?> page-item"><?php echo $list['next']['data']; ?></li>
-	<li class="pagination-end<?php echo $endDisabled; ?> page-item"><?php echo $list['end']['data']; ?></li>
+
+	<?php echo $list['next']['data']; ?>
+	<?php echo $list['end']['data']; ?>
 </ul>

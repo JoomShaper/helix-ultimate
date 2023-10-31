47621ea249ea4e2c9161434fc3c4cd57d902ce5a
MGS Creativa <martin@mgscreativa.com>
2023-10-31 13:29:08 -0300

    Menu, check menu_text if icon class selected parameter

--- plugins/system/helixultimate/src/Core/Classes/HelixultimateMenu.php
+++ plugins/system/helixultimate/src/Core/Classes/HelixultimateMenu.php
@@ -659,7 +659,14 @@ class HelixultimateMenu
 		$class = !empty($class) ? 'class="' . $class . '"' : '';
 
 		if ($item->menu_icon) {
-			$linktitle = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
+			if ($item->getParams()->get('menu_text', 1))
+			{
+				$linktitle = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
+			}
+			else
+			{
+				$linktitle = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span><span class="visually-hidden">' . $item->title . '</span>';
+			}
 		}
 		else if ($item->menu_image)
 		{

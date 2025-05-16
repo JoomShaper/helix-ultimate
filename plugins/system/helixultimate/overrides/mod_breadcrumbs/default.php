9044baa99777d70b1556a92306eb83e17ffeb034
Md Siddiqur Rahman <siddikcoder@gmail.com>
2025-05-16 16:46:02 +0600

    fixed: Structured data json update for breadcrumb

--- plugins/system/helixultimate/overrides/mod_breadcrumbs/default.php
+++ plugins/system/helixultimate/overrides/mod_breadcrumbs/default.php
@@ -9,10 +9,13 @@
 defined ('_JEXEC') or die();
 
 use Joomla\CMS\Language\Text;
+use Joomla\CMS\Router\Route;
+use Joomla\CMS\Uri\Uri;
+use Joomla\CMS\WebAsset\WebAssetManager;
 
 ?>
 
-<ol itemscope itemtype="https://schema.org/BreadcrumbList" class="breadcrumb">
+<ol class="breadcrumb">
 	<?php if ($params->get('showHere', 1)) : ?>
 		<li class="float-start">
 			<?php echo Text::_('MOD_BREADCRUMBS_HERE'); ?>&#160;
@@ -46,20 +49,79 @@ use Joomla\CMS\Language\Text;
 	foreach ($list as $key => $item) :
 		if ($key !== $last_item_key) :
 			if (!empty($item->link)) :
-				$breadcrumbItem = '<a itemprop="item" href="' . $item->link . '" class="pathway"><span itemprop="name">' . $item->name . '</span></a>';
+				$breadcrumbItem = '<a href="' . $item->link . '" class="pathway"><span>' . $item->name . '</span></a>';
 			else :
-				$breadcrumbItem = '<span itemprop="name">' . $item->name . '</span>';
+				$breadcrumbItem = '<span>' . $item->name . '</span>';
 			endif;
 			// Render all but last item - along with separator ?>
-			<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="breadcrumb-item"><?php echo $breadcrumbItem; ?>
-				<meta itemprop="position" content="<?php echo $key + 1; ?>">
-			</li>
+			<li class="breadcrumb-item"><?php echo $breadcrumbItem; ?></li>
 		<?php elseif ($show_last) :
-			$breadcrumbItem = '<span itemprop="name">' . $item->name . '</span>';
+			$breadcrumbItem = '<span>' . $item->name . '</span>';
 			// Render last item if reqd. ?>
-			<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" class="breadcrumb-item active"><?php echo $breadcrumbItem; ?>
-				<meta itemprop="position" content="<?php echo $key + 1; ?>">
-			</li>
+			<li class="breadcrumb-item active"><?php echo $breadcrumbItem; ?></li>
 		<?php endif;
 	endforeach; ?>
 </ol>
+
+<?php
+
+    // Structured data as JSON
+    $data = [
+        '@context'        => 'https://schema.org',
+        '@type'           => 'BreadcrumbList',
+        '@id'             => Uri::root() . '#/schema/BreadcrumbList/' . (int) $module->id,
+        'itemListElement' => []
+    ];
+
+    // Use an independent counter for positions. E.g. if Heading items in pathway.
+    $itemsCounter = 0;
+
+    // If showHome is disabled use the fallback $homeCrumb for startpage at first position.
+    if (isset($homeCrumb)) {
+        $data['itemListElement'][] = [
+                '@type'    => 'ListItem',
+                'position' => ++$itemsCounter,
+                'item'     => [
+                        '@id'  => Route::_($homeCrumb->link, true, Route::TLS_IGNORE, true),
+                        'name' => $homeCrumb->name,
+                ],
+        ];
+    }
+
+    foreach ($list as $key => $item) {
+        // Only add item to JSON if it has a valid link, otherwise skip it.
+        if (!empty($item->link)) {
+            $data['itemListElement'][] = [
+                    '@type'    => 'ListItem',
+                    'position' => ++$itemsCounter,
+                    'item'     => [
+                            '@id'  => Route::_($item->link, true, Route::TLS_IGNORE, true),
+                            'name' => $item->name,
+                    ],
+            ];
+        } elseif ($key === $last_item_key) {
+            // Add the last item (current page) to JSON, but without a link.
+            // Google accepts items without a URL only as the current page.
+            $data['itemListElement'][] = [
+                    '@type'    => 'ListItem',
+                    'position' => ++$itemsCounter,
+                    'item'     => [
+                            'name' => $item->name,
+                    ],
+            ];
+        }
+    }
+
+    if ($itemsCounter) {
+        /** @var WebAssetManager $wa */
+        $wa = $app->getDocument()->getWebAssetManager();
+        $prettyPrint = JDEBUG ? JSON_PRETTY_PRINT : 0;
+        $wa->addInline(
+            'script',
+            json_encode($data, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | $prettyPrint),
+            ['name' => 'inline.mod_breadcrumbs-schemaorg'],
+            ['type' => 'application/ld+json']
+        );
+    }
+?>
+

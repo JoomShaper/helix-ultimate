618f5a25450cf676f06506a7233fe4147cd8d1e9
Susmita Saha <82328288+Susmita-Priya@users.noreply.github.com>
2025-05-27 16:51:12 +0600

    remove meta charset tag

--- plugins/system/helixultimate/overrides/layouts/comingsoon.php
+++ plugins/system/helixultimate/overrides/layouts/comingsoon.php
@@ -44,7 +44,6 @@ $site_title = $app->get('sitename');
 <html class="coming-soon" lang="<?php echo $language; ?>" dir="<?php echo $direction; ?>">
 
 <head>
-	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<?php
 	$theme->head();

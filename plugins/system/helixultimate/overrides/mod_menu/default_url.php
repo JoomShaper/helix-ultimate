ab27a7096d0cd8a548dac514ceb37fe1679d7ca8
Susmita-Priya <susmita.saha.cs@gmail.com>
2025-09-24 17:06:18 +0600

    fix off-canvas and menu-module section

--- plugins/system/helixultimate/overrides/mod_menu/default_url.php
+++ plugins/system/helixultimate/overrides/mod_menu/default_url.php
@@ -8,10 +8,14 @@
 
 defined('_JEXEC') or die;
 
+use Joomla\CMS\Filter\OutputFilter;
 use Joomla\CMS\HTML\HTMLHelper;
-use Joomla\Filter\OutputFilter;
 
 $attributes = [];
+
+$isOffcanvasMenu = $params->get('hu_offcanvas', 0, 'INT') === 1;
+$maxLevel = $params->get('endLevel', 0, 'INT');
+$showToggler = $maxLevel === 0 || $item->level < $maxLevel;
 
 if ($item->anchor_title) {
     $attributes['title'] = $item->anchor_title;
@@ -31,10 +35,10 @@ if ($item->menu_icon) {
     // The link is an icon
     if ($itemParams->get('menu_text', 1)) {
         // If the link text is to be displayed, the icon is added with aria-hidden
-        $linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
+        $linktype = '<span class="pe-2 ' . $item->menu_icon . '" aria-hidden="true"></span>' . $item->title;
     } else {
         // If the icon itself is the link, it needs a visually hidden text
-        $linktype = '<span class="p-2 ' . $item->menu_icon . '" aria-hidden="true"></span><span class="visually-hidden">' . $item->title . '</span>';
+        $linktype = '<span class="pe-2 ' . $item->menu_icon . '" aria-hidden="true"></span><span class="visually-hidden">' . $item->title . '</span>';
     }
 } elseif ($item->menu_image) {
     // The link is an image, maybe with an own class
@@ -51,6 +55,10 @@ if ($item->menu_icon) {
     }
 }
 
+if ($item->parent && $showToggler) {
+    $linktype .= '<span class="menu-toggler"></span>';
+}
+
 if ($item->browserNav == 1) {
     $attributes['target'] = '_blank';
     $attributes['rel'] = 'noopener noreferrer';
@@ -60,8 +68,8 @@ if ($item->browserNav == 1) {
     }
 } elseif ($item->browserNav == 2) {
     $options = 'toolbar=no,location=no,status=no,menubar=no,scrollbars=yes,resizable=yes,' . $params->get('window_open');
-
     $attributes['onclick'] = "window.open(this.href, 'targetWindow', '" . $options . "'); return false;";
 }
 
-echo HTMLHelper::_('link', OutputFilter::ampReplace(htmlspecialchars($item->flink, ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
+echo HTMLHelper::_('link', OutputFilter::ampReplace(htmlspecialchars($item->flink ?? "", ENT_COMPAT, 'UTF-8', false)), $linktype, $attributes);
+?>

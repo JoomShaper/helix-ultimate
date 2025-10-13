478ff0582c334fa8bea02978373ea2f6a265d85a
Md Siddiqur Rahman <53170094+siddik-web@users.noreply.github.com>
2025-10-13 16:32:26 +0600

    fix: update copyright year from 2018 to 2025 in plugin files

--- plugins/system/helixultimate/src/System/HelixCache.php
+++ plugins/system/helixultimate/src/System/HelixCache.php
@@ -2,7 +2,7 @@
 /**
  * @package 	Helix_Ultimate_Framework
  * @author 		JoomShaper <joomshaper@js.com>
- * @copyright 	Copyright (c) 2010 - 2018 JoomShaper
+ * @copyright 	Copyright (c) 2010 - 2025 JoomShaper
  * @license 	http://www.gnu.org/licenses/gpl-2.0.html GNU/GPLv2 or Later
  */
 namespace HelixUltimate\Framework\System;

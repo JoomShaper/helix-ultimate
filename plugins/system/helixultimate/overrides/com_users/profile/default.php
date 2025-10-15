de3c2b78ec8dae9f01bac9f5b30b85f737868ad9
Susmita-Priya <susmita.saha.cs@gmail.com>
2025-10-15 13:04:53 +0600

    Fix: User profile design issues

--- plugins/system/helixultimate/overrides/com_users/profile/default.php
+++ plugins/system/helixultimate/overrides/com_users/profile/default.php
@@ -21,16 +21,6 @@ use Joomla\CMS\Router\Route;
         </div>
     <?php endif; ?>
 
-    <?php if ($this->getCurrentUser()->id == $this->data->id) : ?>
-        <ul class="com-users-profile__edit btn-toolbar float-end">
-            <li class="btn-group">
-                <a class="btn btn-primary" href="<?php echo Route::_('index.php?option=com_users&task=profile.edit&user_id=' . (int) $this->data->id); ?>">
-                    <span class="icon-user-edit" aria-hidden="true"></span> <?php echo Text::_('COM_USERS_EDIT_PROFILE'); ?>
-                </a>
-            </li>
-        </ul>
-    <?php endif; ?>
-
     <?php echo $this->loadTemplate('core'); ?>
     <?php echo $this->loadTemplate('params'); ?>
     <?php echo $this->loadTemplate('custom'); ?>

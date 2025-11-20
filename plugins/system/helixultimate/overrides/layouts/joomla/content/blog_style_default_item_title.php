42ff9fbecb8ecd932bbc52043cd154ae6c4e3aac
Susmita-Priya <susmita.saha.cs@gmail.com>
2025-11-20 17:00:22 +0600

    Fix: replace h2 tag with h4 in article header

--- plugins/system/helixultimate/overrides/layouts/joomla/content/blog_style_default_item_title.php
+++ plugins/system/helixultimate/overrides/layouts/joomla/content/blog_style_default_item_title.php
@@ -24,7 +24,7 @@ $link = RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $d
 <?php if ($displayData->state == 0 || $params->get('show_title') || ($params->get('show_author') && !empty($displayData->author))) : ?>
     <div class="article-header">
         <?php if ($params->get('show_title')) : ?>
-            <h2>
+            <h4>
                 <?php if ($params->get('link_titles') && ($params->get('access-view') || $params->get('show_noauth', '0') == '1')) : ?>
                     <a href="<?php echo Route::_($link); ?>">
                         <?php echo $this->escape($displayData->title); ?>
@@ -32,7 +32,7 @@ $link = RouteHelper::getArticleRoute($displayData->slug, $displayData->catid, $d
                 <?php else : ?>
                     <?php echo $this->escape($displayData->title); ?>
                 <?php endif; ?>
-            </h2>
+            </h4>
         <?php endif; ?>
 
         <?php if ($displayData->state == 0) : ?>
